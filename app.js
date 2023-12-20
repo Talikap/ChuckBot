a9e0553cc091c37f300598091d239712aad0d219
talikap <talikaplan12@gmail.com>
2023-12-20 15:04:13 +0200

    basic bot

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+require('dotenv').config();
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.API_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+
+bot.on('message', (msg) => {
+
+    var Hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+        bot.sendMessage(msg.chat.id, "Hello dear user");
+    }
+
+});
